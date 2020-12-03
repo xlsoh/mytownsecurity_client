78436b99306a5a85fd58c5736e2667bbe012b254
박소현 <sohyeon@bagsohyeon-ui-MacBookPro.local>
2020-12-03 10:39:57 +0900

    [Client/#5/no: nothing changed

--- src/components/modal/user/Login.js
+++ src/components/modal/user/Login.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
 function Login() {
-  return <>로그인 존</>;
+  const [inputValues, setInputValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  return (
+    <>
+      <h1>로그인</h1>
+      <div>
+        <a>ID</a>
+        <input type='text' name='id' placeholder='Enter your ID' />
+        <br />
+        <a>Password</a>
+        <input type='text' name='password' placeholder='Enter your Password' />
+      </div>
+    </>
+  );
 }
 
 export default withRouter(Login);
