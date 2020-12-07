5956d62402d8e2a55eef2bac5a9b23ef8540b050
박소현 <sohyeon@bagsohyeon-ui-MacBookPro.local>
2020-12-07 18:19:54 +0900

    [Client]/#12/ modify: modalP age css 리셋

--- src/styles/Modal.js
+++ src/styles/Modal.js
@@ -16,8 +16,8 @@ const Background = styled.div`
 `;
 
 const ModalWrapper = styled.div`
-  width: 500px;
-  height: 500px;
+  width: 400px;
+  height: 400px;
   box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
   background: #fff;
   color: #000;
