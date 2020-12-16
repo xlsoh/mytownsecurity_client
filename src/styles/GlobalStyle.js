5785d6d11f520038c40fa23d1d5196cc816b252d
yaag19 <m.sun.1290@gmail.com>
2020-12-16 21:57:57 +0900

    [client]/#61/feat: global css 추가

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,10 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
-
+import reset from 'styled-reset';
 export const GlobalStyle = createGlobalStyle`
+${reset};
+a{
+  text-decoration:none;
+  color:inherit;
+}
   * {
       box-sizing: border-box;
       margin: 0px;
       padding: 0px;
       font-family: 'Arial', sans-serif;
   }
+  body{
+    font-size: 14px;
+    background-color:rgba(0,0,0,0);
+}
 `;
