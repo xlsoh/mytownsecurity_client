e31010d315a3b340dfc656b24cf77b0b336f0356
yaag19 <m.sun.1290@gmail.com>
2020-12-16 21:51:09 +0900

    [client]/#39/feat: 검색 좌표 API 컴포넌트 이동 및 좌표변환 함수생성

---
+++ src/components/search/SearchCss.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { darken, lighten } from 'polished';
+
+const StyledButton = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  border: 2px solid; /* 색상 */
+`;
+
+export default Button;
